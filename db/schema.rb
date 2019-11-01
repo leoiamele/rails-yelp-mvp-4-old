ac73f0a64f11ca9e4d6ac21df070a5ec5bae8308
Leandro Iamele <leandroiamele@gmail.com>
2019-11-01 11:29:02 -0300

    change from sqlite to postgrsql database to make it work on heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_10_30_130407) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -24,7 +27,7 @@ ActiveRecord::Schema.define(version: 2019_10_30_130407) do
   create_table "reviews", force: :cascade do |t|
     t.text "content"
     t.integer "rating"
-    t.integer "restaurant_id", null: false
+    t.bigint "restaurant_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
